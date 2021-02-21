ac39e1cdab39c370bf99032d9e94bbd546cf0830
montypadre <montypadre77@gmail.com>
2021-02-20 22:43:24 -0600

    Changed asteroid explosion

---
+++ Assets/Scripts/ExplodeAsteroid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeAsteroid : MonoBehaviour
+{
+    public float minForce;
+    public float maxForce;
+    public float radius;
+
+    void Start()
+    {
+        Explode();
+    }
+    public void Explode()
+    {
+        foreach (Transform t in transform)
+        {
+            var rb = t.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(Random.Range(minForce, maxForce), transform.position, radius);
+            }
+        }
+    }
+}
