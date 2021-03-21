87b118ea1f2cd6065d31b8f68a863a391b0e76b6
montypadre <montypadre77@gmail.com>
2021-03-21 12:51:49 -0500

    Testing wallet integration

---
+++ Assets/Plugins/WebGL/GetWalletAddress.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// use web3.jslib
+using System.Runtime.InteropServices;
+
+public class GetWalletAddress : MonoBehaviour
+{
+    // use WalletAddress function from web3.jslib
+    [DllImport("__Internal")] private static extern string WalletAddress();
+
+    void Start()
+    {
+        Debug.Log(WalletAddress());
+    }
+}
---
+++ Assets/Scripts/GetWalletAddress.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// use web3.jslib
+using System.Runtime.InteropServices;
+
+public class GetWalletAddress : MonoBehaviour
+{
+    // use WalletAddress function from web3.jslib
+    [DllImport("__Internal")] private static extern string WalletAddress();
+
+    void Awake()
+    {
+        Debug.Log(WalletAddress());
+        Debug.Log("Hello");
+    }
+}
